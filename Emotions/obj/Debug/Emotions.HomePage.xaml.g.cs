ba9a2b4389995ad8f1f433ce42674f5f67059587
Soumik Patel <patel.soumik4@gmail.com>
2017-07-13 19:13:59 +1200

    API implemented with exception errors

--- Emotions/obj/Debug/Emotions.HomePage.xaml.g.cs
+++ Emotions/obj/Debug/Emotions.HomePage.xaml.g.cs
@@ -18,9 +18,13 @@ namespace Emotions {
         private global::Xamarin.Forms.Image image;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label TagLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(HomePage));
             image = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "image");
+            TagLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "TagLabel");
         }
     }
 }

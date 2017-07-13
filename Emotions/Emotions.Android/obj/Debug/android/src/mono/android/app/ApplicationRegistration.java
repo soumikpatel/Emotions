718007546120bca9d727b82460b29a68c1bd39dc
Soumik Patel <patel.soumik4@gmail.com>
2017-07-13 19:40:46 +1200

    sln file relocated

---
+++ Emotions/Emotions.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Emotions.Droid.MainApplication, Emotions.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md58eaff4cea28738dda5dbe350a4760c52.MainApplication.class, md58eaff4cea28738dda5dbe350a4760c52.MainApplication.__md_methods);
+		
+	}
+}
